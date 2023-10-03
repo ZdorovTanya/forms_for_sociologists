9ed4ac9c44c311b63b7b1cb93691762ba2c101d5
ZdorovTanya <zdorovilo.tanya@mail.ru>
2023-10-03 13:15:36 +0300

    login frame

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Home from "./component/Pages/Home";
 import Registration from "./component/Pages/Registration";
+import Login from "./component/Pages/Login";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Routes>
           <Route exact path="/" element={<Home />}></Route>
           <Route path="registration" element={<Registration />}></Route>
+          <Route path="login" element={<Login />}></Route>
         </Routes>
       </Router>
     </div>
